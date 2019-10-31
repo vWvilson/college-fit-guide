eb3d079a537cb7f8fc7922dd894ad6eb7daec6c2
Carla Wilson <cjwilson31@gmail.com>
2019-10-31 12:00:02 -0400

    testing

--- pages/out_of_state_three.js
+++ pages/out_of_state_three.js
@@ -1,4 +1,4 @@
-import SchoolList from '../components/SchoolList'
+import IndividualDetails from '../components/IndividualDetails'
 import outside_three from '../data/outside_schools/outside_three'
 import Layout from '../components/MyLayout'
  
@@ -8,42 +8,33 @@ import Layout from '../components/MyLayout'
 const OutsideThree = ()=> {
   
     return(
-        <div>
-            <Layout>
-            <div className = "container">
-            <h1>ACT Score of 22 - 25 & GPA 0f 2.5 - 3.5</h1>
-            <div>
+      <Layout>
+      {outside_three.map(name => <IndividualDetails 
+      key = {name.id}
+      name = {name.name}
+      
+      url = {name.url}
+      private = {name.public}
+      size ={name.size}
+      setting = {name.setting}
+      location = {name.location}
+      iframe = {null}
+      selectivity = {name.selectivity}
+      act = {name.act}
+      reading = {name.reading}
+      math = {name.math}
+      majors = {name.majors}
+      rate = {name.rate}
+      price = {name.netPrice}
+      demographic = {name.demographic}
+      grants = {name.grants}
+      lives = {name.livesOnCampus}
+      programs = {name.programs}
+      alumni  ={name.alumni}
 
-                {outside_three.map(name => <SchoolList 
-                name = {name.name}           
-                />)}
-                </div>
-                </div>
-                
-            </Layout>
-
-            <style jsx>{`
-                  @import url('https://fonts.googleapis.com/css?family=Lato|Oswald|Roboto+Condensed&display=swap');
-
-         
-     h1{
-        color:#82B441;
-        font-size:2.3em;
-        font-family: 'Oswald', sans-serif;
-      }
-      p{
-        font-family: 'Roboto Condensed', sans-serif;
-        font-size:1.3em;
-      }
-            
-      .container {
-        padding:10px;
-      }
-  
-       
-      `}</style>
-        </div>
-
+      
+      />)}   
+     </Layout> 
 
     )
 
